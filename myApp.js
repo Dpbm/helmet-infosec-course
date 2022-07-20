62add9fd4aea2eca935301250cb755c3ac1badb1
Dpbm <dpbm136@gmail.com>
2022-07-20 08:16:53 -0300

    added dns prefetch control

--- myApp.js
+++ myApp.js
@@ -6,11 +6,11 @@ const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter())
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen())
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }))
-
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
 
 
 
